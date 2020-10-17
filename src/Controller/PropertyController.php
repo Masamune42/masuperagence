930346ff1b673cf5eb7f7d5fd3576f0759242c23
Alexandre Caignard <a.caignard@hotmail.fr>
2020-10-17 10:41:26 +0200

    formulaire de contact 8:45

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Entity\PropertySearch;
+use App\Form\ContactType;
 use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -58,23 +60,41 @@ class PropertyController extends AbstractController
      * @param Property $property
      * @return Response
      */
-    public function show(Property $property, string $slug): Response
+    public function show(Property $property, string $slug, Request $request): Response
     {
+
         // Permet de rediriger si le slug (mon-premier-bien) est mal écrit à partir du moment où on réussi à récupérer l'id
         if ($property->getSlug() !== $slug) {
             return $this->redirectToRoute(
                 'property.show',
                 [
-                    'id' => $property->getId(),
-                    'slug' => $property->getSlug()
+                    'id'    => $property->getId(),
+                    'slug'  => $property->getSlug()
                 ],
                 301
             );
         }
+
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre email a bien été envoyé');
+            return $this->redirectToRoute(
+                'property.show',
+                [
+                    'id'    => $property->getId(),
+                    'slug'  => $property->getSlug()
+                ]);
+        }
+
         // $property = $this->repository->find($id);
         return $this->render('property/show.html.twig', [
-            'property' => $property,
-            'current_menu' => 'properties'
+            'property'      => $property,
+            'current_menu'  => 'properties',
+            'form'          => $form->createView()
         ]);
     }
 }
